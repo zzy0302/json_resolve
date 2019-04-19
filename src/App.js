8e664801d5f2f9e678daada8360cf7a2448aba76
zzy0302 <503652983@qq.com>
2019-04-19 09:34:48 +0800

    repire pic 403

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import React, { Component } from 'react';
 import './App.css';
 import data_source from './films.json'
-import { Table, Form, message } from 'antd';
+import { Table, Form, message, LocaleProvider, Input, Button, Icon } from 'antd';
 import {CenterLayout} from "./layout/center-layout";
-
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
 
 class App extends Component {
+  getColumnSearchProps = (dataIndex) => (
+      {
+        filterDropdown: ({
+                           setSelectedKeys, selectedKeys, confirm, clearFilters,
+                         }) => (
+            <LocaleProvider locale={zh_CN}>
+              <div style={{padding: 8}}>
+                <Input
+                    ref={node => {
+                      this.searchInput = node;
+                    }}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+                    style={{width: 188, marginBottom: 8, display: 'block'}}
+                />
+                <Button
+                    type="primary"
+                    onClick={() => this.handleSearch(selectedKeys, confirm)}
+                    icon="search"
+                    size="small"
+                    style={{width: 90, marginRight: 8}}
+                >
+                  Search
+                </Button>
+                <Button
+                    onClick={() => this.handleReset(clearFilters)}
+                    size="small"
+                    style={{width: 90}}
+                >
+                  Reset
+                </Button>
+              </div>
+            </LocaleProvider>
+        ),
+        filterIcon: filtered => <Icon type="search" style={{color: filtered ? '#1890ff' : undefined}}/>,
+        onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+          if (visible) {
+            setTimeout(() => this.searchInput.select());
+          }
+        },
+        // render: (text) => (
+        //     <Highlighter
+        //         highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        //         searchWords={[this.state.searchText]}
+        //         autoEscape
+        //         textToHighlight={text.toString()}
+        //     />
+        // ),
+      });
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({searchText: selectedKeys[0]});
+  };
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({searchText: ''});
+  };
   constructor(props) {
     super(props);
     this.state = {
+      actor: [],
       data: [],
-      columns: [{
+      columns: [
+          {
         title: '',
         dataIndex: 'poster',
-        key: 'poster',
+        key: '_id',
         // eslint-disable-next-line
         render: text => <a><img src={text} width={75} alt={''} referrerPolicy={'never'}/></a>
       },{
         title: '电影名',
         dataIndex: 'title',
         // key: '_id',
+        ...this.getColumnSearchProps('title'),
         // eslint-disable-next-line
         render: text => <a href="javascript:">{text}</a>
       }, {
@@ -35,6 +100,82 @@ class App extends Component {
         title: '类型',
         dataIndex: 'genres',
         // key: 'genres',
+        filters: [
+            {
+          text: '纪录片',
+          value: '纪录片',
+        }, {
+          text: '运动',
+          value: '运动',
+        }, {
+          text: '剧情',
+          value: '剧情',
+        }, {
+          text: '动作',
+          value: '动作',
+        }, {
+          text: '西部',
+          value: '西部',
+        }, {
+          text: '犯罪',
+          value: '犯罪',
+        }, {
+          text: '喜剧',
+          value: '喜剧',
+        }, {
+          text: '科幻',
+          value: '科幻',
+        }, {
+          text: '冒险',
+          value: '冒险',
+        }, {
+          text: '动画',
+          value: '动画',
+        }, {
+          text: '爱情',
+          value: '爱情',
+        }, {
+          text: '恐怖',
+          value: '恐怖',
+        }, {
+          text: '传记',
+          value: '传记',
+        }, {
+          text: '历史',
+          value: '历史',
+        }, {
+          text: '奇幻',
+          value: '奇幻',
+        }, {
+          text: '战争',
+          value: '战争',
+        }, {
+          text: '同性',
+          value: '同性',
+        }, {
+          text: '家庭',
+          value: '家庭',
+        }, {
+          text: '黑色电影',
+          value: '黑色电影',
+        }, {
+          text: '短片',
+          value: '短片',
+        }, {
+          text: '古装',
+          value: '古装',
+        },],
+        onFilter: (value, record) => {
+          // console.log(value);
+          // console.log(record);
+          // let q=[];
+          // for(const i in record) {
+          //   if (record.genres[i].indexOf(value) === 0) {
+          //     q.push(record.genres[i])
+          //   }
+          // }
+          return record.genres.indexOf(value) !== -1
+        },
 
         render: text => {
           let q=[];
@@ -43,6 +184,22 @@ class App extends Component {
             // console.log(i);
             // eslint-disable-next-line
             q.push(<a href="javascript:">{i} </a>);
+
+            // console.log((q))
+          }
+          return q
+        }
+      }, {
+        title: '主演',
+        dataIndex: 'directors',
+        // key: 'pubdate[0]',
+        render: text => {
+          let q=[];
+          // console.log(typeof(q));
+          for(const i of text) {
+            // console.log(i);
+            // eslint-disable-next-line
+            q.push(<a href="javascript:">{i.name} </a>);
 
             // console.log((q))
           }
@@ -111,7 +268,7 @@ class App extends Component {
         <CenterLayout span={16}>
           <Form>
             <Form.Item>
-              <Table columns={this.state.columns} dataSource={this.state.data} pagination={{pageSize:20}} />
+              <Table columns={this.state.columns} dataSource={this.state.data} pagination={{pageSize:10, showSizeChanger:true}} />
             </Form.Item>
           </Form>
         </CenterLayout>
