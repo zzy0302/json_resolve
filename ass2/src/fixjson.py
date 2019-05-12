33175a14c5d51b575f92874b6ee524b48565da51
zzy0302 <503652983@qq.com>
2019-05-12 16:08:02 +0800

    5 threads

--- ass2/src/fixjson.py
+++ ass2/src/fixjson.py
@@ -9,10 +9,16 @@ import requests
 import threading
 from bs4 import BeautifulSoup
 heads = { 'Connection': 'Keep-Alive','Accept': 'text/html, application/xhtml+xml, */*','Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3','Accept-Encoding': 'gzip, deflate','User-Agent': 'Mozilla/6.1 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-with open('qwq.json','r',encoding='UTF-8') as f:
-    l = f.read()
-    j = json.loads(l)
-    for i in range(201,len(j)):
+f = open('qwq.json','r',encoding='UTF-8')
+l = f.read()
+j = json.loads(l)
+f.close()
+start_heap=300
+thread_number=5
+thread_list=[]
+def foo(q):
+    global j
+    for i in list(range(start_heap,len(j)))[q::thread_number]:
         print('Now on',i)
         try:
             r = requests.get(j[i]['poster'])
@@ -23,43 +29,39 @@ with open('qwq.json','r',encoding='UTF-8') as f:
                 time.sleep(random.random())
                 r = requests.get('https://movie.douban.com/subject/' + j[i]['_id'],headers=heads)
                 html = BeautifulSoup(r.content,'lxml')
-                for img in html.find_all('img'):
-                    if 'rel="v:image"' in str(img):
-                        temp=str(img)
-                        img_url = re.findall('src=\"(.*?)\"',temp,re.S|re.M)[0]
-                        j[i]['poster']=img_url
+                j[i]['poster'] = html.find('img', {'rel':'v:image'})['src']
             except Exception as e:
                 print(e)
-        if i % 100 == 0:
-            print('Saving...',i)
-            with open ('new.json','w+') as newf:
-                newf.writelines(json.dumps(j))
-            os.remove('qwq.json')
-            os.rename('new.json', 'qwq.json')
+for i in list(range(thread_number)):
+    try:
+        t = threading.Thread(target=foo,args=(i,))
+        thread_list.append(t)
+        t.start()
+    except Exception as e:
+        pass
+for t in thread_list:
+    t.join()
 
-# print(j[0]['title'])
-# print(requests.get())
-# failed=[]
-# thread_number=500
-# def foo(q):
-#     global failed,j
-#     for i in list(range(len(j)))[q::thread_number]:
-#         try:
-#             download_img.download(j[i]['poster'])
-#             print(i,'successed')
-        # except Exception as e:
-        #     failed.append(j[i])
-# thread_list=[]
-# for i in list(range(thread_number)):
+# j = json.loads(l)
+# for i in range(300,len(j)):
+#     print('Now on',i)
 #     try:
-#         # print(i)
-#         t = threading.Thread(target=foo,args=(i,))
-#         thread_list.append(t)
-#         t.start()
+#         r = requests.get(j[i]['poster'])
+#         r.raise_for_status()
 #     except Exception as e:
-#         pass
-# for t in thread_list:
-#     t.join()
-# with open ('failed.json','w+') as nf:
-#     nf.writelines(json.dumps(j))
+#         try:
+#             print('Doing on',i)
+#             time.sleep(random.random())
+#             r = requests.get('https://movie.douban.com/subject/' + j[i]['_id'],headers=heads)
+#             html = BeautifulSoup(r.content,'lxml')
+#             j[i]['poster'] = html.find('img', {'rel':'v:image'})['src']
+#         except Exception as e:
+#             print(e)
+#     if (i + 1) % 100 == 0:
+#         print('Saving...',i)
 
+
+with open ('new.json','w+') as newf:
+    newf.writelines(json.dumps(j))
+os.remove('qwq.json')
+os.rename('new.json', 'qwq.json')
