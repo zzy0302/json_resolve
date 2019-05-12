85d7383c887205b2a343e80bcee12afaf47ec193
zzy0302 <503652983@qq.com>
2019-05-12 15:15:24 +0800

    qaqaq

--- ass2/src/fixjson.py
+++ ass2/src/fixjson.py
@@ -2,44 +2,41 @@
 import os
 import re
 import json
+import lxml
 import time
-from lxml import etree 
 import random
 import requests
 import threading
-# import download_img
+from bs4 import BeautifulSoup
 heads = { 'Connection': 'Keep-Alive','Accept': 'text/html, application/xhtml+xml, */*','Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3','Accept-Encoding': 'gzip, deflate','User-Agent': 'Mozilla/6.1 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-with open('films.json','r',encoding='UTF-8') as f:
+with open('qwq.json','r',encoding='UTF-8') as f:
     l = f.read()
     j = json.loads(l)
-    # print(j[0])
-    for i in range(len(j)):
-        time.sleep(random.random())
+    for i in range(201,len(j)):
+        print('Now on',i)
         try:
             r = requests.get(j[i]['poster'])
             r.raise_for_status()
         except Exception as e:
             try:
+                print('Doing on',i)
+                time.sleep(random.random())
                 r = requests.get('https://movie.douban.com/subject/' + j[i]['_id'],headers=heads)
-                # html = etree.HTML(str(r.content))
-                html = etree.parse(r.content)
-                print("?")
-                result = html.xpath('//img')
-                # with open ('html.txt','w+') as nf:
-                #     nf.write(html)
-
-                # img_url = html.xpath('nbgnbg')
-                print(result)
-                # img = re.findall('<img(.*?)(src)=\"(?!.*?logo).*',html,re.S|re.M)
-                # print(r.content)
+                html = BeautifulSoup(r.content,'lxml')
+                for img in html.find_all('img'):
+                    if 'rel="v:image"' in str(img):
+                        temp=str(img)
+                        img_url = re.findall('src=\"(.*?)\"',temp,re.S|re.M)[0]
+                        j[i]['poster']=img_url
             except Exception as e:
                 print(e)
-    # print(r.raise_for_status()==None)
-    # if i['title']=='异形大战银河猎人 Alien vs. Hunter':
-    #     url = i['poster']
-    #     print(url)
-    #     r = requests.get(url)
-    #     print(r.raise_for_status()==None)
+        if i % 100 == 0:
+            print('Saving...',i)
+            with open ('new.json','w+') as newf:
+                newf.writelines(json.dumps(j))
+            os.remove('qwq.json')
+            os.rename('new.json', 'qwq.json')
+
 # print(j[0]['title'])
 # print(requests.get())
 # failed=[]
@@ -66,16 +63,3 @@ with open('films.json','r',encoding='UTF-8') as f:
 # with open ('failed.json','w+') as nf:
 #     nf.writelines(json.dumps(j))
 
-    # failed.append(j[2])
-    # failed.append(j[44])
-    # print(json.dumps(failed))
-    # print(j[0]['poster'])
-
-
-
-    # j=[]
-
-    # with open ('new.json','w+') as newf:
-    #     newf.writelines(json.dumps(j))
-# os.remove('films.json')
-# os.rename('new.json', 'films.json')
