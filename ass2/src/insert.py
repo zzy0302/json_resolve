11058825fe03e342ece44d6b0c7dc8269dbc19bf
zzy0302 <503652983@qq.com>
2019-05-13 15:31:18 +0800

    insert test 9

--- ass2/src/insert.py
+++ ass2/src/insert.py
@@ -9,27 +9,27 @@ try:
     db = pymysql.connect("localhost","webass3","zheshimima","webdb" )
     cursor = db.cursor()
     for i in range(1):
-        j[i]['title'] = j[i]['title'].encode('utf-8')
+        j[i]['title'] = j[i]['title']
         j[i]['_id'] = int(j[i]['_id'])
-        j[i]['directors'] = j[i]['directors'][i]['name'].encode('utf-8')
-        j[i]['casts'] = '  '.join([i['name'] for i in j[i]['casts']]).encode('utf-8')
-        j[i]['countries'] = j[i]['countries'][i].encode('utf-8')
+        j[i]['directors'] = j[i]['directors'][i]['name']
+        j[i]['casts'] = '  '.join([i['name'] for i in j[i]['casts']])
+        j[i]['countries'] = j[i]['countries'][i]
         j[i]['duration'] = int(j[i]['duration'])
-        j[i]['genres'] = ' '.join([i for i in j[i]['genres']]).encode('utf-8')
-        j[i]['languages'] = j[i]['languages'][i].encode('utf-8')
-        j[i]['pubdate'] = j[i]['pubdate'][i].encode('utf-8')
+        j[i]['genres'] = ' '.join([i for i in j[i]['genres']])
+        j[i]['languages'] = j[i]['languages'][i]
+        j[i]['pubdate'] = j[i]['pubdate'][i]
         j[i]['rating'] = float(j[i]['rating']['average'])
-        j[i]['summary'] = j[i]['summary'].encode('utf-8')
-        j[i]['year'] = j[i]['year'].encode('utf-8')
+        j[i]['summary'] = j[i]['summary']
+        j[i]['year'] = j[i]['year']
         print(j[0])
         # print(j[i], end = '\n')
         # print(j[i]['title'],j[i]['_id'],j[i]['poster'],j[i]['casts'],j[i]['countries'],j[i]['directors'],j[i]['duration'],\
         #     j[i]['genres'],j[i]['languages'],j[i]['pubdate'],j[i]['rating'],j[i]['summary'],j[i]['year'])
         cursor.execute('insert into filmstable ( movie_name, movie_id, movie_poster, movie_casts, movie_countries, \
                         movie_directors, movie_duration, movie_genres, movie_languages, movie_pubdate, movie_rating, \
-                        movie_summary, movie_year )  values ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',\
-                        [j[i]['title'],j[i]['_id'],j[i]['poster'],j[i]['casts'],j[i]['countries'],j[i]['directors'],j[i]['duration'],\
-                        j[i]['genres'],j[i]['languages'],j[i]['pubdate'],j[i]['rating'],j[i]['summary'],j[i]['year']])
+                        movie_summary, movie_year )  values ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'(\
+                        j[i]['title'],j[i]['_id'],j[i]['poster'],j[i]['casts'],j[i]['countries'],j[i]['directors'],j[i]['duration'],\
+                        j[i]['genres'],j[i]['languages'],j[i]['pubdate'],j[i]['rating'],j[i]['summary'],j[i]['year']))
     db.commit()
     db.close()
 except Exception as e:
