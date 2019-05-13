664261ec450e5a6235a94bd8a6f8f72daa39e47f
zzy0302 <503652983@qq.com>
2019-05-13 15:37:25 +0800

    insert test 11

--- ass2/src/insert.py
+++ ass2/src/insert.py
@@ -25,6 +25,11 @@ try:
         # print(j[i], end = '\n')
         # print(j[i]['title'],j[i]['_id'],j[i]['poster'],j[i]['casts'],j[i]['countries'],j[i]['directors'],j[i]['duration'],\
         #     j[i]['genres'],j[i]['languages'],j[i]['pubdate'],j[i]['rating'],j[i]['summary'],j[i]['year'])
+        print('insert into filmstable ( movie_name, movie_id, movie_poster, movie_casts, movie_countries, \
+                        movie_directors, movie_duration, movie_genres, movie_languages, movie_pubdate, movie_rating, \
+                        movie_summary, movie_year )  values ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'%(\
+                        j[i]['title'],j[i]['_id'],j[i]['poster'],j[i]['casts'],j[i]['countries'],j[i]['directors'],j[i]['duration'],\
+                        j[i]['genres'],j[i]['languages'],j[i]['pubdate'],j[i]['rating'],j[i]['summary'],j[i]['year']))
         cursor.execute('insert into filmstable ( movie_name, movie_id, movie_poster, movie_casts, movie_countries, \
                         movie_directors, movie_duration, movie_genres, movie_languages, movie_pubdate, movie_rating, \
                         movie_summary, movie_year )  values ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'%(\
